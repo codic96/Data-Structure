8c962e5c393450b42a3eb10192c059545d6f25b1
Vipin Kumar <88959266+codic96@users.noreply.github.com>
2022-03-27 10:58:29 +0530

    Update Array.py

--- Array.py
+++ Array.py
@@ -1,6 +1,16 @@
+''' Python Array Modules '''
+
 import array
-int_array = array.array('i',[1,2,3,4,5,6])
-float_array = array.array('f',[1.0,1.1,1.2,1.3,1.4,1.5])
-print(int_array)
+int_array = array.array('i',[1,2,3,4,5,6]) #Creating array int array
+float_array = array.array('f',[1.0,1.1,1.2,1.3,1.4,1.5]) # Creating float float array
+print(int_array) #Print array
 print(float_array)
 
+'''We can print array elements using for elements '''
+
+import array
+
+int_arrary = array.array('i',[1,2,3,4,5,6])
+for a in int_array:
+  print(a)
+  
