d8541c7f666f4a5fc7bca119101c6ed0ac75167d
Vipin Kumar <88959266+codic96@users.noreply.github.com>
2022-03-30 11:16:48 +0530

    Update Array.py

--- Array.py
+++ Array.py
@@ -48,6 +48,64 @@ except ValueError as ve:
   print(int_array[3:])
   print(int_array[1:])
   
+  '''Searching an element in the Array '''
+  
+  import array 
+  int_array = array.array('i',[0,1,2,3,4,5,6])
+  print(f' 1 is found at index {int_array.index(1)}')
+  try:
+    print(int_array.index(20))
+ except valueError as ev:
+    print(ev)
+   
+ 
+''' Updating value  at specific index ''' 
+import array
+int_array = array.array('i',[0,1,2,3,4,5])
+print(int_array)
+int_array[0] = -1
+int_array[1] = -2
+int_array[2] = -3
+print(int_array)
+
+try:
+  int_array[10]=-10
+except IndexError as ve:
+  print(ve)
+
+''' Reversing an Array '''
+
+import array 
+int_array = array.array('i',[0,1,2,3,4,5])
+print(int_array)
+print(int_array.reverse())
+
+''' Count of the occurence of an element '''
+
+import array 
+int_array = array.array('i',[0,1,2,3,4,5])
+print(int_array.count(1))
+print(int_array.count(0))
+
+
+''' Extending an array by Appending an Iterate '''
+
+import array
+int_array1 = array.array('i',[0,1,2,3,4,5])
+int_array2 = array.array('i',[0,2,23,133,13])
+int_array1.extend(int_array2)
+
+''' Converting Array to List '''
+
+import array 
+int_array = array.array('i',[0,1,2,3,4,5,6,7])
+print(int_array.toList())
+
+
+  
+
+  
+  
   
 
 
