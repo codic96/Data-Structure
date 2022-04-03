c07ce69759f1e36b1fb713a6f3b5a9806762a3ab
Vipin Kumar <88959266+codic96@users.noreply.github.com>
2022-04-03 10:17:17 +0530

    Update LinkedList.py

--- LinkedList.py
+++ LinkedList.py
@@ -23,5 +23,24 @@ class LinkedList:
     print(linked_list.head.item,end='')
     linked_list.head = linked_list.head.next
     
+    
+    #Second Example 
+    #create a node
+    class Node:
+      def __init__(self,data):
+        self.data=data
+        self.next=None
+    class LinkedList:
+      def __init__(self):
+        self.head = None
+      #Insert at the beginning 
+      
+      def insertAtBeginning(self,new_data):
+        new_node=Node(new_data)
+        new_node.next =self.head
+        self.head = new_node 
+        
+        
+    
   
     
