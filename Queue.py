586e5f0ca3b108b75c1ca1c71e2a674caf0aca45
Vipin Kumar <88959266+codic96@users.noreply.github.com>
2022-04-11 11:59:29 +0530

    Create Queue.py

---
+++ Queue.py
@@ -0,0 +1,7 @@
+wmt_stock_exchange = []
+wmt_stock_exchange.insert(0,131.10)
+wmt_stock_exchange.insert(0,132.12)
+wmt_stock_exchange.insert(0,133.10)
+wmt_stock_exchange.insert(0,134.23)
+print(wmt_stock_exchange)
+wmt_stock_exchange.pop()
