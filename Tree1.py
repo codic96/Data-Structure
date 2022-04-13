638e917ab5e0742bae29c839cfff44d5aa4b4728
Vipin Kumar <88959266+codic96@users.noreply.github.com>
2022-04-13 12:17:11 +0530

    Update Tree1.py

--- Tree1.py
+++ Tree1.py
@@ -5,4 +5,15 @@ class Node:
     self.left=None
     self.right=None
     self.val =item
+def inorder(root):
+  if root:
+    #Traverse left
+    inorder(root.left)
+    #Traverse right
+    print(str(root.val)+ "->",end='')
+    #Traverse right
+    inorder(root.right)
+    
+    
+    
     
