5073ece189bf7c457638f3b8bd8b27ac452e0a65
Vipin Kumar <88959266+codic96@users.noreply.github.com>
2022-04-19 12:25:56 +0530

    Create Recurrsion.py

---
+++ Recurrsion.py
@@ -0,0 +1,10 @@
+def factorial(x):
+  """ This is a recursion function to find the factorial of an interger """
+  if x==1:
+    return 1
+  else:
+    return (x*factorial(x-1))
+
+num = 3
+print("The factorial of",num,"is",factorial(num))
+  
