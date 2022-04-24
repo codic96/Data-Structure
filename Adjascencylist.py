6a30dd1d052cd3b1ff6435c5e563d4b6c6b23fa7
Vipin Kumar <88959266+codic96@users.noreply.github.com>
2022-04-24 14:57:40 +0530

    Update Adjascencylist.py

--- Adjascencylist.py
+++ Adjascencylist.py
@@ -4,4 +4,18 @@ class AdjNode:
   def __init__(self,vales):
     self.vertices = vales
     self.next = None
+class Graph:
+  def __init__(self,num):
+    self.v = num
+    self.graph=[None]*self.v
+  #Add edges
+  
+  def add_edges(self,s,d):
+    node = AdjNode(d)
+    node.next = self.graph[s]
+    self.graph[s]=node
+    node = AdjNode(s)
+    node.next = self.graph[d]
+    self.graph[d]=node 
+    
     
